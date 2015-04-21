8639916ce88b6be7fddc123d4dff3bdbbdc996d2
Matt Luesing <Kainte@gmail.com>
2015-04-20 17:20:12 -0700

    Initial Commit

---
+++ src/main/java/org/nationsatwar/palette/Palette.java
@@ -0,0 +1,37 @@
+package org.nationsatwar.palette;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+ 
+@Mod(modid = Palette.MODID, name = Palette.MODNAME, version = Palette.MODVER)
+public class Palette {
+
+    @Instance(Palette.MODID)
+    public static Palette instance;
+	
+	public static final String MODID = "palette";
+	public static final String MODNAME = "Palette";
+	public static final String MODVER = "0.0.1";
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		
+		
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		
+		
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+		
+		
+	}
+}
